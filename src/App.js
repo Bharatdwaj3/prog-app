b99420c9616e843a463c4524608f92cb6dc02702
Bharatdwaj3 <joshijai2002@gmail.com>
2024-03-15 15:19:27 +0530

    "initial comment"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import ProgressBar from './progress-bar.components';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+
+
 
 function App() {
+
+  const [completed, setCompleted]=useState(0);
+  
+useEffect(()=>{
+  setInterval(()=>setCompleted(Math.floor(Math.random()*100)+1),2000);
+},[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProgressBar bgcolor={"#6a1b9a"} completed={completed}/>
     </div>
   );
 }
